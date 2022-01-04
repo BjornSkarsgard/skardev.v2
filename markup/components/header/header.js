031c144d032c71f1682aa2515476963e64cca723
unknown <bjornskarsgarddev@gmail.com>
2022-01-04 22:51:42 +0500

    header

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,1 +1,9 @@
-
+import $ from "jquery";
+$('body').click( function() {
+    if($('.header').hasClass('header--scroll')) {
+        $('.header').removeClass('header--scroll')
+    }
+    else {
+        $('.header').addClass('header--scroll')
+    }
+})
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import "./libraries/libraries.js"
+
+import "./../../components/header/header"
 /*
     This file can be used as entry point for webpack!
  */
